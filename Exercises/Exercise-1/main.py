0d84dc109582f370c493f5dd1a6d625180c6b8f0
Pavol-69 <137830124+Pavol-69@users.noreply.github.com>
2025-11-11 23:01:18 +0100

    3/5

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import os.path
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -12,8 +14,20 @@ download_uris = [
 
 
 def main():
-    # your code here
-    pass
+    # Création du dossier downloads s'il n'existe pas
+    path = "downloads"
+    if os.path.exists(path) :
+        print("Chemin" , path, "existe déjà")
+    else:
+        os.mkdir(path)
+        print("Chemin" , path, "créé")
+
+    # Téléchargement de chacun des fichiers
+    for i in range(len(download_uris)) :
+        response = requests.get(download_uris[i])
+        name = download_uris[i].split("/")[-1]
+        with open(path + "/" + name, "wb") as f:
+            f.write(response.content)
 
 
 if __name__ == "__main__":
