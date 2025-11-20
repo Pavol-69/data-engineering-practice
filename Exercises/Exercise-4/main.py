440dba343cb422acf579ccb743ea774efd836465
Pavol-69 <137830124+Pavol-69@users.noreply.github.com>
2025-11-20 21:33:35 +0100

    exo4

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,10 +1,38 @@
-import boto3
+import os
+import json
+import csv
 
+def main() :
+    jsonList = []
 
-def main():
-    # your code here
-    pass
+    # Parcours de tous les répertoires
+    for (root,dirs,files) in os.walk(os.getcwd(),topdown=True):
 
+        # On regarde s'il y a des fichiers, puis s'ils se terminent par ".json"
+        if len(files) > 0 :
+            for file in files :
+                if file.endswith('.json') :
 
-if __name__ == "__main__":
+                    # On ouvre le fichier, et on ajoute le contenu à notre liste
+                    jsonList.append(json.load(open(root + "/" + file)))
+    
+    # Création du fichier .csv
+    df = open('data_file.csv', 'w')
+
+    # Création du writer
+    cw = csv.writer(df)
+    c = 0
+    for jsonObject in jsonList :
+        if c == 0 :
+
+            # Writing headers of CSV file
+            h = jsonObject.keys()
+            cw.writerow(h)
+            c += 1
+
+        # Writing data of CSV file
+        cw.writerow(jsonObject.values())
+    df.close()
+
+if __name__ == "__main__" :
     main()
